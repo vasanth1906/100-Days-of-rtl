a5796369b90a2a364c247d80da12d67a540da430
Sai Vasanth <151848951+vasanth1906@users.noreply.github.com>
2024-01-03 02:30:27 +0530

    Update srlatch.v

--- DAY15 SR LATCH/srlatch.v
+++ DAY15 SR LATCH/srlatch.v
@@ -1,6 +1,9 @@
 module sr_latch(S, R, Q, Q_n);
-input logic S, R;
-output logic Q, Q_n;
-nor i1(Q, R, Q_n);
-nor i2(Q_n, S, Q);
+
+	input logic S, R;
+	output logic Q, Q_n;
+	
+	nor #10 i1(Q, R, Q_n);
+	nor #10 i2(Q_n, S, Q);
+	
 endmodule
