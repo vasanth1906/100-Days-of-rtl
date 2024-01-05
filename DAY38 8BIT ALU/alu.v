8f360498be7e63c705eec18ed6a00da3d61d81d2
Sai Vasanth <151848951+vasanth1906@users.noreply.github.com>
2024-01-05 10:51:38 +0530

    Update alu.v

--- DAY38 8BIT ALU/alu.v
+++ DAY38 8BIT ALU/alu.v
@@ -1,44 +1,27 @@
-module alu_tb;
-
-	// Inputs
-	reg [3:0] a;
-	reg [3:0] b;
-	reg [2:0] opcode;
-
-	// Outputs
-	wire [7:0] rslt;
-
-	// Instantiate the Unit Under Test (UUT)
-	alu uut (
-		.a(a), 
-		.b(b), 
-		.opcode(opcode), 
-		.rslt(rslt)
-	);
-
-	
-	initial 
+module alu (a, b, opcode, rslt);
+input [3:0] a, b;
+input [2:0] opcode;
+output [7:0] rslt;
+reg [7:0] rslt;
+parameter add_op = 3'b000,
+sub_op = 3'b001,
+mul_op = 3'b010,
+and_op = 3'b011,
+or_op = 3'b100,
+not_op = 3'b101, //negation
+xor_op = 3'b110,
+xnor_op = 3'b111;
+always @ (a or b or opcode)
 begin
-#0 a = 4'b0001; b = 4'b0010; opcode = 3'b000;
-#10 a = 4'b0110; b = 4'b0110; opcode = 3'b000;
-#10 a = 4'b1100; b = 4'b0011; opcode = 3'b001;
-#10 a = 4'b1101; b = 4'b1010; opcode = 3'b001;
-#10 a = 4'b1100; b = 4'b0111; opcode = 3'b010;
-#10 a = 4'b1111; b = 4'b0011; opcode = 3'b010;
-#10 a = 4'b1100; b = 4'b0111; opcode = 3'b011;
-#10 a = 4'b1101; b = 4'b1011; opcode = 3'b011;
-#10 a = 4'b0101; b = 4'b1011; opcode = 3'b100;
-#10 a = 4'b1001; b = 4'b1010; opcode = 3'b100;
-#10 a = 4'b1001; opcode = 3'b101;
-#10 a = 4'b0011; opcode = 3'b101;
-#10 a = 4'b0111; b = 4'b1011; opcode = 3'b110;
-#10 a = 4'b1010; b = 4'b0101; opcode = 3'b110;
-#10 a = 4'b0110; b = 4'b0110; opcode = 3'b111;
-#10 a = 4'b0011; b = 4'b1110; opcode = 3'b111;
+case (opcode)
+add_op: rslt = a + b;
+sub_op: rslt = a - b;
+mul_op: rslt = a * b;
+and_op: rslt = a & b; //also ab
+or_op: rslt = a | b;
+not_op: rslt = ~a; //also ~b
+xor_op: rslt = a ^ b;
+xnor_op: rslt = ~(a ^ b);
+endcase
 end
-initial
-begin
-	$monitor(" A=%b | B=%b | Opcode = %b | Result=%b",a,b,opcode,rslt);
-	#300 $finish;
-	end      
 endmodule
