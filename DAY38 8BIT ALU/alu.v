25cc596420719682d4e0314d3442483578e5d316
Sai Vasanth <151848951+vasanth1906@users.noreply.github.com>
2024-01-05 10:50:32 +0530

    Create alu.v

---
+++ DAY38 8BIT ALU/alu.v
@@ -0,0 +1,44 @@
+module alu_tb;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+	reg [2:0] opcode;
+
+	// Outputs
+	wire [7:0] rslt;
+
+	// Instantiate the Unit Under Test (UUT)
+	alu uut (
+		.a(a), 
+		.b(b), 
+		.opcode(opcode), 
+		.rslt(rslt)
+	);
+
+	
+	initial 
+begin
+#0 a = 4'b0001; b = 4'b0010; opcode = 3'b000;
+#10 a = 4'b0110; b = 4'b0110; opcode = 3'b000;
+#10 a = 4'b1100; b = 4'b0011; opcode = 3'b001;
+#10 a = 4'b1101; b = 4'b1010; opcode = 3'b001;
+#10 a = 4'b1100; b = 4'b0111; opcode = 3'b010;
+#10 a = 4'b1111; b = 4'b0011; opcode = 3'b010;
+#10 a = 4'b1100; b = 4'b0111; opcode = 3'b011;
+#10 a = 4'b1101; b = 4'b1011; opcode = 3'b011;
+#10 a = 4'b0101; b = 4'b1011; opcode = 3'b100;
+#10 a = 4'b1001; b = 4'b1010; opcode = 3'b100;
+#10 a = 4'b1001; opcode = 3'b101;
+#10 a = 4'b0011; opcode = 3'b101;
+#10 a = 4'b0111; b = 4'b1011; opcode = 3'b110;
+#10 a = 4'b1010; b = 4'b0101; opcode = 3'b110;
+#10 a = 4'b0110; b = 4'b0110; opcode = 3'b111;
+#10 a = 4'b0011; b = 4'b1110; opcode = 3'b111;
+end
+initial
+begin
+	$monitor(" A=%b | B=%b | Opcode = %b | Result=%b",a,b,opcode,rslt);
+	#300 $finish;
+	end      
+endmodule
