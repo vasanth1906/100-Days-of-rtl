71ef69d195392e55c784c5cc1df87285bd1a074d
Sai Vasanth <151848951+vasanth1906@users.noreply.github.com>
2024-01-05 12:11:48 +0530

    Create mealy_tb.v

---
+++ DAY47 MEALY OVERLAPPING/mealy_tb.v
@@ -0,0 +1,37 @@
+module test_bench;
+reg din,clk,reset;
+wire y;
+
+fsm_11001 dut(din,clk,reset,y);
+
+initial begin
+clk= 1'b0;
+forever #5 clk= ~clk;
+end
+
+initial begin
+reset= 1'b1;
+#10 reset= 1'b0;
+#5 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b1;
+#10 din= 1'b1;
+#10 din= 1'b0;
+#10 din= 1'b0;
+#10 din= 1'b1;
+end
+
+initial begin
+$monitor("\t\t clock: %d  input: %d   detect: %d",clk, din, y);
+#180 $finish;
+end
+endmodule
